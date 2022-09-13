e1b77c7842ca018d830e017bf8d1ab4570901194
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2022-09-13 06:09:54 -0700

    Git Repo Created

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Ecomm-Site</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
